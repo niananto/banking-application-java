2e48494a1eae7fd6ec7cd459b9f35c61c40d4385
Nazmul Islam Ananto <nazmulislamananto@gmail.com>
2024-02-02 02:39:14 +0600

    Account Class. Needs a null object

---
+++ src/Account.java
@@ -0,0 +1,53 @@
+import java.time.LocalDate;
+
+public abstract class Account {
+    protected String name;
+    protected int accountNumber;
+    protected LocalDate dateOfCreation;
+    protected double balance;
+
+    protected Account(String name, int accountNumber, LocalDate dateOfCreation, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.dateOfCreation = dateOfCreation;
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "name='" + name + '\'' +
+                ", accountNumber=" + accountNumber +
+                ", dateOfCreation=" + dateOfCreation +
+                ", balance=" + balance +
+                '}';
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public Boolean deposit(double depositAmount) {
+        if (depositAmount < 0) {
+            System.out.println("Deposit amount cannot be negative");
+            return false;
+        }
+        this.balance += depositAmount;
+        System.out.println("Deposited " + depositAmount + " into account " + this.accountNumber);
+        return true;
+    }
+
+    public Boolean withdraw(double withdrawAmount) {
+        if (withdrawAmount < 0) {
+            System.out.println("Withdraw amount cannot be negative");
+            return false;
+        }
+        if (withdrawAmount > this.balance) {
+            System.out.println("Withdraw amount cannot be greater than balance");
+            return false;
+        }
+        this.balance -= withdrawAmount;
+        System.out.println("Withdrew " + withdrawAmount + " from account " + this.accountNumber);
+        return true;
+    }
+}
